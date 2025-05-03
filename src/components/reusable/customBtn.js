2a15613288e9f27f1bf4a7cb653bcb04ead279e0
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2025-05-04 01:12:50 +0500

    chore:custom btn

---
+++ src/components/reusable/customBtn.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CustomBtn = (props) => {
+  const { btnText, border, width } = props;
+  return (
+    <button
+      className={`py-[13px] bg-(--primary) rounded-${border} text-center text-white cursor-pointer w-[${width}]`}
+    >
+      {btnText}
+    </button>
+  );
+};
+
+export default CustomBtn;
