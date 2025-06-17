c9b5d6138c421988b9eaa958d9b60767110d737d
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2025-06-17 15:36:25 +0500

    doc reg

--- src/data/doctorProfile.js
+++ src/data/doctorProfile.js
@@ -1,3 +1,27 @@
-export const stepper = [
-
-]
+export const stepper = {
+  createProfile: [
+    {
+      heading: "Create Profile",
+      step1: "signup",
+      step2: "Join as doctor",
+      step3: "Registration",
+    },
+  ],
+  doc_details: [
+    {
+      pic: "",
+      name: "Dr. Smith Brown",
+      designation: "Orthopaedic Surgeon",
+      qualification: "MBBS, MD",
+      location: "Sydney, Australia",
+      rating: 5,
+      exp: 42,
+      sub_spec: "Foot and ankle; Hand",
+      hospital: "Apollo hospital, Sydney",
+      email: "brown.smith@gmail.com",
+      fee: 500,
+      online_fee: 500,
+      clinic_loc: "123 Maple Street, Apollo hospital, Springfield, Sydney",
+    },
+  ],
+};
