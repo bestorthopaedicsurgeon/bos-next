84dab1200d5b10c36fc08550bdd0eb9a0bfb73d5
Huzaifa134 <mhuzaifa134@gmail.com>
2025-08-18 02:33:27 +0500

    blog card fix

--- src/components/blogPage/BlogCard.js
+++ src/components/blogPage/BlogCard.js
@@ -17,10 +17,10 @@ export const BlogCard = (card) => {
   };
 
   return (
-    <div className="flex flex-col items-start rounded-lg border bg-white p-5">
+    <div className="flex flex-col items-start rounded-lg border bg-white">
       {card?.image && (
         <div
-          className="mb-4 h-[200px] w-full rounded-lg bg-contain bg-center bg-no-repeat sm:h-[220px] md:h-[240px] object-cover"
+          className="mb-4 h-[200px] w-full rounded-t-lg bg-cover bg-center bg-no-repeat sm:h-[220px] md:h-[240px]"
           style={{
             backgroundImage: `url(${card.image})`,
           }}
@@ -28,18 +28,18 @@ export const BlogCard = (card) => {
           aria-label={card.title}
         />
       )}
-      <h3 className="text-primary">{card.title}</h3>
-      <p className="mb-4 text-sm text-[#515151]">
+      <h3 className="text-primary p-5">{card.title}</h3>
+      <p className="mb-4 text-sm text-[#515151] px-5">
         {formatDate(card.createdAt)} • 11 min read
       </p>
-      <p className="mb-4 line-clamp-3">{card.description}</p>
       <Link
           // onClick={() => {
           //   redirect(`/blog/${card.slug}`);
           // }}
         href={`/blog/${card.slug}`}
-        className="text-primary cursor-pointer text-lg font-bold"
+        className="text-primary cursor-pointer text-lg font-bold px-5 pb-10"
         target="_blank"
+
       >
         Read More →
       </Link>
