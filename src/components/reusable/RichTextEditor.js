a87abed28080b8f494efcbb3416ffcc5cd3112b5
rabiahmed8 <ahmedrabi8@gmail.com>
2025-11-25 21:40:23 +0500

    env change

--- src/components/reusable/RichTextEditor.js
+++ src/components/reusable/RichTextEditor.js
@@ -9,7 +9,7 @@ export default function RichTextEditor({ value, onChange }) {
   return (
     <div style={{ width: '100%', maxWidth: '100%' }}>
       <Editor 
-        apiKey={process.env.TINYMCE_API_KEY || 'no-api-key'}
+        apiKey={process.env.NEXT_PUBLIC_TINYMCE_API_KEY || 'no-api-key'}
         onInit={(evt, editor) => (editorRef.current = editor)}
         value={value}
         init={{
